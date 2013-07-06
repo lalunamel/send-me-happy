1e69f6e33e16d1a31e154e7bbc292c81552ae86d
Cody Sehl <cody.sehl@gmail.com>
2013-07-06 14:41:19 -0600

    [Finishes #52940103] implementing interpolation in message sending service

--- app/services/message_sender_service.rb
+++ app/services/message_sender_service.rb
@@ -1,29 +1,42 @@
 class MessageSenderService
-		def initialize(args = {})
-			@user = args[:user]
-			@template = args[:template]
-		end
+	# TODO Combine initialize and deliver_message
+	def initialize(args = {})
+		@user = args[:user]
+		@template = args[:template]
+		@interpolation = args[:interpolation] || {}
+	end
 
-		# sends a message by setting up, calling send_sms, then handling the response
-		def deliver_message
-			text = @template.text
-			sent_sms = send_sms("+#{@user.phone}", text)
+	# sends a message by setting up, calling send_sms, then handling the response
+	def deliver_message
+		text = interpolate_text @template.text
+		sent_sms = send_sms("+#{@user.phone}", text)
 
-			if(sent_sms.status != "failed")
-				message = Message.create!(user: @user, template: @template, text: text)
-				true	
-			else
-				logger.info "Message failed to send: #{sent_sms}"
-				false
-			end
+		if(sent_sms.status != "failed")
+			message = Message.create!(user: @user, template: @template, text: text)
+			true	
+		else
+			logger.info "Message failed to send: #{sent_sms}"
+			false
 		end
-		
+	end
+	
 private
-		def send_sms(phone, text)
-			TWILIO.account.sms.messages.create(
-				from: TWILIO_FROM,
-				to: phone,
-				body: text
-			)
+	def send_sms(phone, text)
+		TWILIO.account.sms.messages.create(
+			from: TWILIO_FROM,
+			to: phone,
+			body: text
+		)
+	end
+
+	def interpolate_text(text)
+		modified_text = text
+		if(@interpolation.present?)
+	    @interpolation.each do |key, value|
+	      modified_text = modified_text.gsub(key.to_s, value)
+	    end
 		end
+
+		modified_text
+	end
 end
--- spec/services/message_sender_service_spec.rb
+++ spec/services/message_sender_service_spec.rb
@@ -1,20 +1,56 @@
 require "spec_helper"
 
 describe MessageSenderService do
-	it "should deliver a message to a user's phone number" do
+	before :each do
 		mocked_twilio_message = double("twilio message", status: "queued")
 		MessageSenderService.any_instance.stub(:send_sms) { mocked_twilio_message }
 
-		user = create :user
-		template = create :template
-
-		MessageSenderService.new(user: user, template: template).deliver_message
-
-		sent_message = Message.first
-
-		expect(Message.all.count).to eq 1
-		expect(sent_message.text).to eq template.text
-		expect(sent_message.user).to eq user
-		expect(sent_message.template).to eq template 
+		@user = create :user
+		@template = create :template
 	end
+
+	describe "sending a message" do
+		it "should deliver a message to a user's phone number" do
+			@service = MessageSenderService.new(user: @user, template: @template)
+			@service.deliver_message
+
+			sent_message = Message.first
+
+			expect(Message.all.count).to eq 1
+			expect(sent_message.text).to eq @template.text
+			expect(sent_message.user).to eq @user
+			expect(sent_message.template).to eq @template 
+		end
+
+		it "should send an message with interpolated text" do
+			template = create :template, text: "hello _foo_ world"
+			@service = MessageSenderService.new(user: @user, template: template, interpolation: { _foo_: "foo" })
+			@service.stub(:send_sms) { double("twilio message", status: "queued") }
+			
+			@service.should_receive(:send_sms).with("+#{@user.phone}", "hello foo world")
+
+			@service.deliver_message
+		end
+		
+	end
+	
+
+	describe "#interpolate_text" do
+		before :each do
+			@interpolation = { _foo_bar_: "hello world" }
+			@input = "Hello my _foo_bar_ name is"
+			@expected_output = "Hello my hello world name is"
+		end
+
+		it "should replace given symbols with given text" do
+			@service = MessageSenderService.new(user: @user, template: @template, interpolation: @interpolation)
+			expect(@service.send(:interpolate_text, @input)).to eq @expected_output
+		end
+
+		it "should return the same string if no interpolation hash is given" do
+			@service = MessageSenderService.new(user: @user, template: @template, interpolation: {})
+			expect(@service.send(:interpolate_text, @expected_output)).to eq @expected_output
+		end
+	end
+	
 end
